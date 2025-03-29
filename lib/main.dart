149a29b9bad28c2c126ee422c225804fc3963390
Paras <102306336+parthm223@users.noreply.github.com>
2025-03-29 05:34:09 +0530

    error fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sky_techiez/screens/home_screen.dart';
-import 'package:sky_techiez/screens/welcome_screen.dart';
+import 'package:get_storage/get_storage.dart';
+
+import 'package:sky_techiez/screens/splas_screen.dart';
+
 import 'package:sky_techiez/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -17,7 +20,7 @@ class MyApp extends StatelessWidget {
       title: 'Sky Techiez',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkTheme(),
-      home: const WelcomeScreen(),
+      home: const SplashScreen(),
       // home: const HomeScreen(),
     );
   }
